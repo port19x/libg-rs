7105db06adf3718bae37b348680ad8db267bcfb0
port19 <port19@port19.xyz>
2024-03-18 11:02:08 +0100

    `nucleo-matcher` -> `fuzzy-select` + usage example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use reqwest::blocking::Client;
+use fuzzy_select::FuzzySelect;
 
 fn main() {
+    // Reqwest how to
     let request = Client::new()
         .get("https://port19.xyz");
 
@@ -10,4 +12,12 @@ fn main() {
         Ok(response) => println!("{}", response.status()),
         Err(_err) => todo!(),
     }
+
+    //fuzzy_select how to
+    let options = vec!["vanilla", "strawberry", "chocolate"];
+    let selected = FuzzySelect::new()
+        .with_prompt("What's your favorite flavor of ice cream?")
+        .with_options(options)
+        .select();
+    println!("\nYour favorite ice cream flavor is {:?}\n", selected);
 }
