adc085786d5c0fa2d55f4ef678193fa19fc454f7
port19 <port19@port19.xyz>
2024-03-18 22:49:19 +0100

    point towards the libgen endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 //use fuzzy_select::FuzzySelect;
 
-fn libgrequest (endpoint:&str) -> Result<String, reqwest::Error> {
+fn libgsearch (endpoint:&str) -> Result<String, reqwest::Error> {
     // enpoint := what comes after the / of the url. e.g.: https://libgen.rs/enpoint has the endpoint /enpoint
     // TODO Make function actually fail over to alternatives: libgen.is or libgen.st at Error match or non-200 status
-    //http://libgen.rs/search.php?res=100&req=harry
-    let response = reqwest::blocking::get("https://port19.xyz")?.error_for_status();
+    let base = "https://libgen.rs/search.php?res=100&req=";
+    let url = format!("{}{}", base, endpoint);
+    let response = reqwest::blocking::get(url)?.error_for_status();
     return Ok(response?.text()?);
 }
 
 fn main() {
-    let x = libgrequest("/");
+    let x = libgsearch("harry");
     let y = match x {
         Ok(y) => y,
         Err(y) => todo!(),
