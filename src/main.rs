ccfa821070d72ce5df724bcd79e273d1a78a7eda
port19 <port19@port19.xyz>
2024-03-20 11:37:39 +0100

    `unhtml` -> `scraper`

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,5 @@
 //use fuzzy_select::FuzzySelect;
-use unhtml::FromHtml;
-
-
-#[derive(FromHtml)]
-struct SearchTable {
-    //path e.g. .c > tbody:nth-child(1) > tr:nth-child(1)
-    #[html(attr="valign")]
-    entries: Vec<SearchResult>,
-}
-
-// <tr valign=top bgcolor=#C6DEFF><td>58419</td>
-#[derive(FromHtml)]
-struct SearchResult {
-    #[html(selector = "tr:nth-child(1)", attr = "inner")]
-    id: i32,
-    #[html(selector = "tr:nth-child(2)", attr = "inner")]
-    //TODO This no workey because stupid hrefs
-    author: String,
-    #[html(selector = "tr:nth-child(3)", attr = "inner")]
-    //TODO Again no workey....
-    title: String,
-    #[html(selector = "tr:nth-child(4)", attr = "inner")]
-    publisher: String,
-    #[html(selector = "tr:nth-child(5)", attr = "inner")]
-    year: i32,
-    #[html(selector = "tr:nth-child(6)", attr = "inner")]
-    pages: i32,
-    #[html(selector = "tr:nth-child(7)", attr = "inner")]
-    language: String,
-    #[html(selector = "tr:nth-child(8)", attr = "inner")]
-    file_size: String,
-    #[html(selector = "tr:nth-child(9)", attr = "inner")]
-    file_format: String,
-    #[html(selector = "tr:nth-child(10)", attr = "inner")]
-    //TODO deal with lé href again
-    dl_link: String,
-}
+use scraper::{Html, Selector};
 
 fn libgsearch (endpoint:&str) -> Result<String, reqwest::Error> {
     // enpoint := what comes after the / of the url. e.g.: https://libgen.rs/enpoint has the endpoint /enpoint
@@ -52,10 +16,12 @@ fn main() {
         Ok(y) => y,
         Err(_y) => todo!(),
     };
+    let document = Html::parse_document(&y);
+    let selector = Selector::parse(".c").unwrap();
+    let search_table = document.select(&selector);
+    //let main_table = document.select()
 
-    let r1 = SearchTable::from_html(&y);
-
-    println!("{}", y);
+    println!("{:#?}", search_table);
 
     // Fuzzy_select How To
     // let options = vec!["vanilla", "strawberry", "chocolate"];
