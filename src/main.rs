6b82630922ea4976ed44867160e5da2a5a92388f
port19 <port19@port19.xyz>
2024-03-20 13:55:27 +0100

    made a working search result struct. imperfections remain ofc

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,20 @@ fn libgsearch (endpoint:&str) -> Result<String, reqwest::Error> {
     return Ok(response?.text()?);
 }
 
+#[derive(Debug)]
+struct SearchResult {
+    id: String,
+    author: String,
+    title: String,
+    publisher: String,
+    year: String,
+    pages: String,
+    language: String,
+    file_size: String,
+    file_format: String,
+    dl_link: String,
+}
+
 fn main() {
     let x = libgsearch("harry");
     let y = match x {
@@ -23,9 +37,24 @@ fn main() {
     let select_rows = Selector::parse("tr").unwrap();
     //Note: skip1 to skip the table header that is unfortunately not marked via <th>
     let mut row_iterator = search_table.select(&select_rows).skip(1);
-    let row1 = row_iterator.next().unwrap().html();
+    let row1 = row_iterator.next().unwrap();
 
     println!("{:#?}", row1);
+
+    let sr1 = SearchResult {
+        id:          row1.child_elements().nth(0).unwrap().inner_html().to_string(),
+        author:      row1.child_elements().nth(1).unwrap().inner_html().to_string(), //TODO
+        title:       row1.child_elements().nth(2).unwrap().inner_html().to_string(), //TODO
+        publisher:   row1.child_elements().nth(3).unwrap().inner_html().to_string(),
+        year:        row1.child_elements().nth(4).unwrap().inner_html().to_string(),
+        pages:       row1.child_elements().nth(5).unwrap().inner_html().to_string(),
+        language:    row1.child_elements().nth(6).unwrap().inner_html().to_string(),
+        file_size:   row1.child_elements().nth(7).unwrap().inner_html().to_string(),
+        file_format: row1.child_elements().nth(8).unwrap().inner_html().to_string(),
+        dl_link:     row1.child_elements().nth(9).unwrap().inner_html().to_string(), //TODO
+    };
+
+    println!("{:#?}", sr1);
 
     // Fuzzy_select How To
     // let options = vec!["vanilla", "strawberry", "chocolate"];
