54053ce7ee80cc0ecc494943646aeadbc1c36c33
port19 <port19@port19.xyz>
2024-03-20 16:15:19 +0100

    a beautiful `libgsearch` function emerges

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
 //use fuzzy_select::FuzzySelect;
 use scraper::{ElementRef, Html, Selector};
 
-fn libgsearch (endpoint:&str) -> Result<String, reqwest::Error> {
-    // enpoint := what comes after the / of the url. e.g.: https://libgen.rs/enpoint has the endpoint /enpoint
-    // TODO Make function actually fail over to alternatives: libgen.is or libgen.st at Error match or non-200 status
-    let base = "https://libgen.rs/search.php?res=100&req=";
-    let url = format!("{}{}", base, endpoint);
-    let response = reqwest::blocking::get(url)?.error_for_status();
-    return Ok(response?.text()?);
-}
-
 #[derive(Debug)]
 struct SearchResult {
     id: String,
@@ -39,22 +30,24 @@ fn tr_to_searchresult (tr:ElementRef) -> SearchResult {
     };
 }
 
-fn main() {
-    let x = libgsearch("harry");
-    let y = match x {
-        Ok(y) => y,
-        Err(_y) => todo!(),
-    };
-    let document = Html::parse_document(&y);
+fn libgsearch (searchterm:&str) -> Vec<SearchResult> {
+    let base = "https://libgen.rs/search.php?res=100&req=";
+    let url = format!("{}{}", base, searchterm);
+    let response = reqwest::blocking::get(url).unwrap().error_for_status().unwrap().text().unwrap();
+
+    let document = Html::parse_document(&response);
     let toplevel_selector = Selector::parse(".c > tbody").unwrap();
     let search_table = document.select(&toplevel_selector).next().unwrap();
 
     let select_rows = Selector::parse("tr").unwrap();
-    //Note: skip1 to skip the table header that is unfortunately not marked via <th>
-    let mut row_iterator = search_table.select(&select_rows).skip(1);
-    let mut rowstructs = row_iterator.map(tr_to_searchresult);
+    let row_iterator = search_table.select(&select_rows).skip(1); //Note: skip(1) skips the table header
+    let rowstructs = row_iterator.map(tr_to_searchresult).collect();
+    return rowstructs;
+}
 
-    println!("{:#?}", rowstructs.nth(0));
+fn main() {
+    let x = libgsearch("harry");
+    println!("{:#?}", x);
 
     // Fuzzy_select How To
     // let options = vec!["vanilla", "strawberry", "chocolate"];
